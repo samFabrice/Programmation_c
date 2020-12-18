bcafd69c3d76e2c58dd185b060f76f9f799d3f65
Fabrice dedo <dsamfabrice5@gmail.com>
2020-12-18 19:46:21 +0100

    Correction de la fonction alea.c (Il reste encore deux ou trois choses à corriger à cette fonction

--- alea.c
+++ alea.c
@@ -4,14 +4,14 @@
 
 void alea (char mondeCarre[30][30],  int dim , float cpt)
 {
-    char joueur_alea; 
-    joueur_alea = '#' +rand()%2;
-    if (joueur_alea == '#' )
-    {
-    printf("Le joueur aléatoire a jouer le joueur_1\n");
+    cpt = 0;
+    char ordinateur;
+    ordinateur = '#' +rand()%2;
+    printf("L'ordinateur a joué %c\n", ordinateur);
     char couleur_aleatoire ;
+    printf("L'ordinateur va choisir une couleur\n");
     couleur_aleatoire ='A'+rand()%7;
-    printf("Le joueur aléatoire a choisit la couleur : %c \n", couleur_aleatoire );
+    printf("L'ordinateur a choisi : %c \n", couleur_aleatoire );
     bool changed = true;
      while (changed  == true )
               {
@@ -20,84 +20,29 @@ void alea (char mondeCarre[30][30],  int dim , float cpt)
                     {
                         for (int j = 29; j>0; j--)
                             {
-                                if ( mondeCarre[i][j] == joueur_alea)
+                                if ( mondeCarre[i][j] == ordinateur)
                                     {    
                                         if ( j+1 <=  29 && mondeCarre[i][j+1] ==  couleur_aleatoire )  
                                                 {
-                                                    mondeCarre[i][j+1] = joueur_alea;
+                                                    mondeCarre[i][j+1] = ordinateur;
                                                     changed = changed | true;
                                                     cpt ++;
                                                 }
                                         if ( j-1 >= 0 && mondeCarre[i][j-1] ==  couleur_aleatoire ) 
                                                 { 
-                                                    mondeCarre[i][j-1] = joueur_alea;
+                                                    mondeCarre[i][j-1] = ordinateur;
                                                     changed = changed | true;
                                                     cpt ++;
                                                 }
                                         if (i-1 >= 0 && mondeCarre[i-1][j] ==  couleur_aleatoire )  
                                                 {
-                                                    mondeCarre[i-1][j] = joueur_alea;
+                                                    mondeCarre[i-1][j] = ordinateur;
                                                     changed = changed | true;
                                                     cpt ++;
                                                 }
                                         if (i+1 <= 29 && mondeCarre[i+1][j] ==  couleur_aleatoire )  
                                                 {
-                                                    mondeCarre[i+1][j] = joueur_alea;
-                                                    changed = changed | true;
-                                                    cpt++;
-                                                }
-                                        else 
-                                                {
-                                                    changed =  false;
-                                                    
-                                                }
-                                    
-                                    } 
-                            }
-                    }
-            }
-            
-            cpt = (cpt / 900)*100;
-            printf("%f\n", cpt);
-    }
-    
-    if (joueur_alea == '$')
-    {
-    printf("Le joueur aléatoire a jouer le joueur_2\n");
-    char couleur_aleatoire ;
-    couleur_aleatoire ='A'+rand()%7;
-    printf("Le joueur aléatoire a choisit la couleur : %c \n", couleur_aleatoire );
-    bool changed = true;
-     while (changed  == true )
-              {
-                changed = false;
-                for (int i =0; i<30; i++)
-                    {
-                        for (int j = 29; j>0; j--)
-                            {
-                                if ( mondeCarre[i][j] == joueur_alea)
-                                    {    
-                                        if ( j+1 <=  29 && mondeCarre[i][j+1] ==  couleur_aleatoire )  
-                                                {
-                                                    mondeCarre[i][j+1] = joueur_alea;
-                                                    changed = changed | true;
-                                                    cpt ++;
-                                                }
-                                        if ( j-1 >= 0 && mondeCarre[i][j-1] ==  couleur_aleatoire ) 
-                                                { 
-                                                    mondeCarre[i][j-1] = joueur_alea;
-                                                    changed = changed | true;
-                                                    cpt ++;
-                                                }
-                                        if (i-1 >= 0 && mondeCarre[i-1][j] ==  couleur_aleatoire )  
-                                                {
-                                                    mondeCarre[i-1][j] = joueur_alea;
-                                                    changed = changed | true;
-                                                    cpt ++;
-                                                }
-                                        if (i+1 <= 29 && mondeCarre[i+1][j] ==  couleur_aleatoire )  
-                                                {
-                                                    mondeCarre[i+1][j] = joueur_alea;
+                                                    mondeCarre[i+1][j] = ordinateur;
                                                     changed = changed | true;
                                                     cpt ++;
                                                 }
@@ -111,8 +56,6 @@ void alea (char mondeCarre[30][30],  int dim , float cpt)
                             }
                     }
             }
-            
             cpt = (cpt / 900)*100;
             printf("%f\n", cpt);
-    }
 }
