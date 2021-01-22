820213060afea2b0d4af1eb3cfb4a3cd328f328d
Fabrice dedo <dsamfabrice5@gmail.com>
2021-01-22 22:20:02 +0100

    Version finale du joueur_glouton_prevoyant. Je l'ai tester en jouant contre le joueur glouton simple et à chaque fois sur 40 parties je joueur glouton_prevoyant a remporter le jeu le plus de fois

--- joueur_glouton_prevoyant.c
+++ joueur_glouton_prevoyant.c
@@ -8,12 +8,16 @@
 
 void glouton_prevoyant(char mondeCarre[30][30],  int dim)
 {
-    char joueur_glouton_prevoyant= '^';
+   char joueur_glouton_prevoyant= '@';
   float score_joueur_glouton_prevoyant = 0;
     printf("joueur_glouton_prevoyant = %c \n", joueur_glouton_prevoyant);
     char couleur_aleatoire;
-    char tab[1000] = {0,0,0,0,0,0,0,0}; // On initialise un tableau dans lequel on stoque les couleurs qui qui peuvent ajouter des cases à la zone du joueur
+   char tab[1000] = {0}; // On initialise un tableau dans lequel on stoque les couleurs qui qui peuvent ajouter des cases à la zone du joueur
     int o = 0;
+    int a = 0;
+    
+    while (a<10)
+    {
                 for (int i =0; i<30; i++)
                     {
                         for (int j = 0; j<30; j++)
@@ -24,76 +28,45 @@ void glouton_prevoyant(char mondeCarre[30][30],  int dim)
                                                 {
                                                     tab[o] = 'A';
                                                     o ++;
-                                                        if ( ( i+1<= 29 && j+1 <=29 && mondeCarre[i+1][j+1] == 'A') || (i+1<= 29 && j-1>=0 && mondeCarre[i+1][j-1] == 'A') || ( i-1>= 0 && j+1 <=29 && mondeCarre[i-1][j+1] == 'A') || (i-1>= 0 && j-1>=0 && mondeCarre[i-1][j-1] == 'A'))
-                                                        {
-                                                            tab[o] = 'A';
-                                                            o++;
-                                                            
-                                                        }
+                                                  
                                                     
                                                 }
                                    if ( (j+1 <=  29 && mondeCarre[i][j+1] ==  'B' ) || (j-1 >= 0 && mondeCarre[i][j-1] ==  'B') || (i+1 <= 29 && mondeCarre[i+1][j] == 'B' ) || (i-1 >= 0 && mondeCarre[i-1][j] ==  'B') ) 
                                                 { 
                                                     tab[o] = 'B' ;
                                                     o ++;
-                                                          if ( ( i+1<= 29 && j+1 <=29 && mondeCarre[i+1][j+1] == 'B') || (i+1<= 29 && j-1>=0 && mondeCarre[i+1][j-1] == 'B') || ( i-1>= 0 && j+1 <=29 && mondeCarre[i-1][j+1] == 'B') || (i-1>= 0 && j-1>=0 && mondeCarre[i-1][j-1] == 'B'))
-                                                        {
-                                                            tab[o] = 'B';
-                                                            o++;
-                                                            
-                                                        }
+                                                    
                                                 }
                                   if ( (j+1 <=  29 && mondeCarre[i][j+1] ==  'C' ) || (j-1 >= 0 && mondeCarre[i][j-1] ==  'C') || (i+1 <= 29 && mondeCarre[i+1][j] == 'C' ) || (i-1 >= 0 && mondeCarre[i-1][j] ==  'C') ) 
                                                 {
                                                     tab[o] = 'C' ;
                                                     o ++;
-                                                      if ( ( i+1<= 29 && j+1 <=29 && mondeCarre[i+1][j+1] == 'C') || (i+1<= 29 && j-1>=0 && mondeCarre[i+1][j-1] == 'C') || ( i-1>= 0 && j+1 <=29 && mondeCarre[i-1][j+1] == 'C') || (i-1>= 0 && j-1>=0 && mondeCarre[i-1][j-1] == 'C'))
-                                                        {
-                                                            tab[o] = 'C';
-                                                            o++;
-                                                            
-                                                        }
+                                                     
                                                 }
                                  if ( (j+1 <=  29 && mondeCarre[i][j+1] ==  'D' ) || (j-1 >= 0 && mondeCarre[i][j-1] ==  'D') || (i+1 <= 29 && mondeCarre[i+1][j] == 'D' ) || (i-1 >= 0 && mondeCarre[i-1][j] ==  'D') ) 
                                                 {
                                                     tab[o] = 'D' ;
                                                     o ++;
-                                                      if ( ( i+1<= 29 && j+1 <=29 && mondeCarre[i+1][j+1] == 'D') || (i+1<= 29 && j-1>=0 && mondeCarre[i+1][j-1] == 'D') || ( i-1>= 0 && j+1 <=29 && mondeCarre[i-1][j+1] == 'D') || (i-1>= 0 && j-1>=0 && mondeCarre[i-1][j-1] == 'D'))
-                                                        {
-                                                            tab[o] = 'D';
-                                                            o++;
-                                                        }
+
                                                 }
                                 if ( (j+1 <=  29 && mondeCarre[i][j+1] ==  'E' ) || (j-1 >= 0 && mondeCarre[i][j-1] ==  'E') || (i+1 <= 29 && mondeCarre[i+1][j] == 'E' ) || (i-1 >= 0 && mondeCarre[i-1][j] ==  'E') ) 
                                                 {
                                                     tab[o] = 'E';
                                                     o ++;
-                                                         if ( ( i+1<= 29 && j+1 <=29 && mondeCarre[i+1][j+1] == 'E') || (i+1<= 29 && j-1>=0 && mondeCarre[i+1][j-1] == 'E') || ( i-1>= 0 && j+1 <=29 && mondeCarre[i-1][j+1] == 'E') || (i-1>= 0 && j-1>=0 && mondeCarre[i-1][j-1] == 'E'))
-                                                        {
-                                                            tab[o] = 'E';
-                                                            o++;  
-                                                        }
+
                                                     
                                                 }
                                  if ( (j+1 <=  29 && mondeCarre[i][j+1] ==  'F' ) || (j-1 >= 0 && mondeCarre[i][j-1] ==  'F') || (i+1 <= 29 && mondeCarre[i+1][j] == 'F' ) || (i-1 >= 0 && mondeCarre[i-1][j] ==  'F') ) 
                                                 {
                                                     tab[o] = 'F';
                                                     o ++;
-                                                     if (( i+1<= 29 && j+1 <=29 && mondeCarre[i+1][j+1] == 'F') || (i+1<= 29 && j-1>=0 && mondeCarre[i+1][j-1] == 'F') || ( i-1>= 0 && j+1 <=29 && mondeCarre[i-1][j+1] == 'F') || (i-1>= 0 && j-1>=0 && mondeCarre[i-1][j-1] == 'F'))
-                                                        {
-                                                            tab[o] = 'F';
-                                                            o++;
-                                                        }
+
                                                 }
                                 if ( (j+1 <=  29 && mondeCarre[i][j+1] ==  'G' ) || (j-1 >= 0 && mondeCarre[i][j-1] ==  'G') || (i+1 <= 29 && mondeCarre[i+1][j] == 'G' ) || (i-1 >= 0 && mondeCarre[i-1][j] ==  'G') ) 
                                                 {
                                                     tab[o] = 'G';
                                                     o ++;
-                                                     if (( i+1<= 29 && j+1 <=29 && mondeCarre[i+1][j+1] == 'G') || (i+1<= 29 && j-1>=0 && mondeCarre[i+1][j-1] == 'G') || ( i-1>= 0 && j+1 <=29 && mondeCarre[i-1][j+1] == 'G') || (i-1>= 0 && j-1>=0 && mondeCarre[i-1][j-1] == 'G'))
-                                                        {
-                                                            tab[o] = 'G';
-                                                            o++;  
-                                                        }
+                                                    
                                                 }
                               
                               }
@@ -104,11 +77,9 @@ void glouton_prevoyant(char mondeCarre[30][30],  int dim)
                             }
                     }
                     
-                        
-                 /*  for(int i =0; i < 100; i++)
-                            {                                       
-                                printf("%c ", tab[i]);
-                            }*/
+                     a ++;   
+}                        
+           
             
             couleur_aleatoire = loi_du_plus_fort2(tab,10);
             
@@ -177,6 +148,9 @@ void glouton_prevoyant(char mondeCarre[30][30],  int dim)
                 score_joueur_glouton_prevoyant ++;
         }
     }
+    
+
+
           score_joueur_glouton_prevoyant =   (score_joueur_glouton_prevoyant/900)*100;
         printf("score_joueur_glouton_prevoyant = %f\n", score_joueur_glouton_prevoyant);
     
